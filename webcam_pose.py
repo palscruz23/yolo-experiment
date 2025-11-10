a2e08cd6fce6faae457e45f6d4f67adf43ebaaff
Pol John Cruz <poljohncruz@gmail.com>
2025-11-10 19:33:39 +1030

    added webcam pose feature

--- webcam_pose.py
+++ webcam_pose.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import cv2
 import os
+import time
 
 # Add folders if does not exist
 if not os.path.exists('data'):
@@ -9,6 +10,8 @@ if not os.path.exists('output'):
     os.mkdir('output')
 
 model = YOLO('yolov8n-pose.pt')
+
+cap = cv2.VideoCapture(0)
 
 keypoints = {
     0: 'nose',
@@ -29,12 +32,31 @@ keypoints = {
     15: 'left_ankle',
     16: 'right_ankle',
 }
+fps_list = []
+while True:
+    start_time = time.time()
+    ret, frame = cap.read()
+    if not ret:
+        print("failed to grab frame")
+        break
 
+    results = model(frame, verbose=False)
+    annotated_frame = results[0].plot()
 
-model.predict(source=0, 
-              show=True,
-              save=True,
-              project='output',
-              name='cam',
-              exist_ok=True,
-              )
+    cv2.imshow('Pose Estimation', annotated_frame)
+
+    
+    fps = 1 / (time.time() - start_time)
+    fps_list.append(fps)
+    if len(fps_list) > 20:
+        fps_list.pop(0)
+    avg_fps = sum(fps_list) / len(fps_list)
+    cv2.putText(annotated_frame, f'FPS: "{avg_fps:.1f}',
+                (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow("YOLO phone camera", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
